2b07c14799fce36722fb2d7c8b55cc6b5f61cf5e
EderCard <edersoncard@gmail.com>
2020-06-04 14:26:18 -0400

    Add Lab01_HelloWorld

--- Lab01_HelloWorld/Class1.cs
+++ Lab01_HelloWorld/Class1.cs
@@ -3,10 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Autodesk.AutoCAD.ApplicationServices;
+using Autodesk.AutoCAD.EditorInput;
+using Autodesk.AutoCAD.Runtime;
+
 
 namespace Lab01_HelloWorld
 {
     public class Class1
     {
+        [CommandMethod("HW")]
+        public void HelloWorld()
+        {        
+            Editor ed = Application.DocumentManager.MdiActiveDocument.Editor;
+            ed.WriteMessage("Hello World");
+        }
     }
 }
